ad505ad50038d6ebcd2fd4f75f9fdd5f6dea7a2f
aCetylAcid <aka.l.m1ura.setsuna.karimofu@gmail.com>
2015-07-02 19:05:56 +0900

    ADD sample

---
+++ LoginSampleApp/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  LoginSampleApp
+//
+//  Created by aCetylAcid on 2015/07/02.
+//  Copyright (c) 2015年 zrn-ns.com. All rights reserved.
+//
+
+import UIKit
+import KeychainAccess
+
+class LoginViewController : UIViewController {
+    
+    @IBOutlet weak var textFieldId: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    
+    @IBAction func tappedSave(sender: AnyObject) {
+        let keychain = Keychain()
+        
+        keychain["user_id"]   = textFieldId.text
+        keychain["user_pass"] = textFieldPassword.text
+    }
+    
+    @IBAction func tappedClear(sender: AnyObject) {
+        let keychain = Keychain()
+        
+        textFieldId.text       = nil
+        textFieldPassword.text = nil
+        keychain["user_id"]   = nil
+        keychain["user_pass"] = nil
+    }
+    
+    override func viewDidLoad() {
+        let keychain = Keychain()
+        
+        textFieldId.text       = keychain["user_id"]
+        textFieldPassword.text = keychain["user_pass"]
+    }
+}
